a4db7358e227e5759c430e4d3dc6627e16946350
Stefan Yordanov <stefan_yordanov7@abv.bg>
2023-02-26 15:29:08 +0200

    Book Library

---
+++ src/main/java/com/example/Library/web/PagesController.java
@@ -0,0 +1,16 @@
+package com.example.Library.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class PagesController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
